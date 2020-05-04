468aa464a92fa549f82a7e5948e5876147bd2f51
Ruben Ramirez <rramirez110911@gmail.com>
2020-05-03 23:08:52 -0700

    a lot of changes

--- code-challenges.js
+++ code-challenges.js
@@ -24,7 +24,7 @@ const boilingPoint = (temp) => {
     }
 }
 // log it to the console
-console.log(boilingPoint(temp3));
+// console.log(boilingPoint(temp3));
 
 
 
@@ -33,7 +33,19 @@ console.log(boilingPoint(temp3));
 
 var myNumbers1 = [3, 7, 0, 6, -9]
 
-
+// create the function called multiply that takes in an array and multiplies each number in the array by 5
+const multiply = (array) => {
+    // create a variable that will hold the new array of multiplied values
+    let newArray = []
+    //create the for loop the will iterate through the array
+    for(let i = 0; i < array.length; i++ ) {
+        newArray.push(array[i] * 5)
+    }
+    // return statement that will return the updated array
+    return newArray
+}
+// log the test
+// console.log(multiply(myNumbers1));
 
 
 // --------------------3) Create a function that multiplies each number in the array by 5 using map.
@@ -41,8 +53,13 @@ var myNumbers1 = [3, 7, 0, 6, -9]
 
 var myNumbers2 = [8, -7, 0, 6, 2]
 
-
-
+// create the function that takes an array as an argument
+const mapMultiply = (array) => {
+    //return the array passed as a parameter using the .map function
+    return array.map(value => value * 5)
+}
+// log into console to test
+// console.log(mapMultiply(myNumbers2))
 
 // --------------------4) Create a function that removes all the vowels from a string.
 // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
@@ -50,8 +67,14 @@ var myNumbers2 = [8, -7, 0, 6, 2]
 var stringWithVowels1 = "HeyThereLearnStudent"
 var stringWithVowels2 = "ILoveJavaScript"
 
+// const deVowelizer = (string)  => {
+//     let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+//     let newArry = string.split("")
 
+//     return newArry.filter(value => vowels.indexOf(value) === -1).join("")
+// }
 
+// console.log(deVowelizer(stringWithVowels2));
 
 
 // --------------------5) Expand the logic from #4 to inform your user if the variable passed into the vowel removal function is not a string.
@@ -60,16 +83,44 @@ var stringWithVowels2 = "ILoveJavaScript"
 var notAString1 = true
 var notAString2 = 42
 
+// const deVowelizer = (string)  => {
 
+//     if( typeof string === "string") {
+//         return "true"
+//     } else if (typeof string !== "string") {
+//         return "not a string"
+//     }
+//     let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+//     let newArry = string.split("")
+        
+//     return newArry.filter(value => vowels.indexOf(value) === -1).join("")
+// }
 
+// console.log(deVowelizer(notAString1))
 
 // --------------------6) Create a function that takes the toonimals array and returns an array with only the toon objects that are cats.
 // Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
 
-var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
+var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}] 
 
+// create function that takes in an array as an argument
+// const catify = (array) => {
+//      return array.filter(value => value.animal === "cat")
+// }
 
+// console.log(catify(toonimals))
+// use filter to iterate and filter out the non cat objects
 
 // --------------------7) Using the toonimals variable from #6, create a function that returns only the names of the non-cats.
 // Expected output: "Itchy" "Daffy" "Ren"
+
+const catify = (array) => {
+     array.filter(value => {
+         if (value.animal !== "cat") {
+              console.log(value.name);
+         }
+        })
+}
+
+console.log(catify(toonimals)) 
--- interview-questions.md
+++ interview-questions.md
@@ -4,64 +4,77 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: a function is a block of code that executes any code found inside it's code block. You would use a function to have reusable code that you can call.
 
   Researched answer:
 
+  set of statements that performs a task or calculates a value. It allows you to define a block of code, give it a name and then execute it as many times as you want.
 
 
 2. What is the difference between map and filter?
 
-  Your answer:
+  Your answer: Filter is used to remove items from the array whereas map just iterates over it.
 
   Researched answer:
 
-
+  The map() method creates a new array with the results of calling a function for every array element. The filter() method creates an array filled with all array elements that pass a test implemented by the provided function
 
 3. What is the difference between console.log() and return?
 
-  Your answer:
+  Your answer: return is returning a value from a function. Console.log is printing what ever is inside the paranthesis into the console.
 
   Researched answer:
 
+console.log will display the parameter passed to the log method in the console window.
 
+he return statement, the function will stop executing, and return the specified value.
 
 4. In regards to functions, what is an argument?
 
   Your answer:
 
+  A value you pass into a function.
+
   Researched answer:
 
-
+Function arguments are the real values passed to (and received by) the function.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
   Your answer:
 
+  driver writes the code and checks for syntax errors
+  the navigator guides them and focuses on the logic.
+
   Researched answer:
 
-
+The driver is the one with their hands on the keyboard. They are responsible for entering information and following the instructions given by the navigator. This approach requires constant communication. And talking about your code will make you a better developer and a better teammate. It is important to switch roles every 10-20 minutes and take frequent break
 
 6. What is TDD? Describe the work flow associated with TDD.
 
-  Your answer:
+  Your answer: testing your code after every few lines of code. write the tests and then fill it in with your code.
 
   Researched answer:
 
-
+Test-driven development (TDD) is a software development process that relies on the repetition of a very short development cycle: requirements are turned into very specific test cases, then the code is improved so that the tests pass.
 
 7. What is something we did in class this week you found helpful?  
 
   Your answer:
 
-
+One of the most helpful things is giving examples of challenges of the code after lecture  or before doing the challenges.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-- React
-- Yarn
-- React State
-- CRUD
-- this
+- React - a javascript library that was developed by Facebook for building user interfaces.
+
+- Yarn - it is a package manager that lets you install certain thing faster.
+
+- React State - a javascript object holds information and changes over time.
+
+- CRUD - create, read, update, delete. a model for building we applications.
+
+
+- this - a keyword that lets you reference the object from the inside without haveing to write the object name.
